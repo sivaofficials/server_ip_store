551f17ec8acfb3b1294bcc5abe0b20b2258ab703
MonkNo1 <hitman20025@gmail.com>
2024-02-23 09:48:13 +0530

    Auth Schema

--- src/mongo_connect.js
+++ src/mongo_connect.js
@@ -1,42 +1,46 @@
 const mongoose = require("mongoose");
 
 //connect server
-mongoose.connect('mongodb://127.0.0.1:27017/server_ip')
-  
+mongoose.connect("mongodb://127.0.0.1:27017/server_ip");
 
 const UserSchema = new mongoose.Schema({
-    IP: String,
-    localname: String,
-    domain: String,
-    username: String,
-    password: String,
-    st:String,
-})
-const Ip =mongoose.model("ip",UserSchema)
+  IP: String,
+  localname: String,
+  domain: String,
+  username: String,
+  password: String,
+  st: String,
+});
+const Ip = mongoose.model("ip", UserSchema);
+
+const SecuritySchema = new mongoose.Schema({
+  uname: String,
+  paswrd: String,
+  toks: String,
+});
+const sec = mongoose.model("security", SecuritySchema);
 
 exports.store = async (data) => {
-    const newperson = new Ip(data);
-    await newperson.save();
-  };
+  const newperson = new Ip(data);
+  await newperson.save();
+};
 
-  exports.getdata = async () => {
-    const app1 = await Ip.find({});
-    return app1;
-  };
+exports.getdata = async () => {
+  const app1 = await Ip.find({});
+  return app1;
+};
 
-  exports.getmodal = async (ip) => {
-    const app2 = await Ip.find({IP:ip});
-    return app2;
-  };
+exports.getmodal = async (ip) => {
+  const app2 = await Ip.find({ IP: ip });
+  return app2;
+};
 
-  exports.reports_del = async (ip) => {
-    const app1 = await Ip.deleteOne({ IP: ip })
-        .then(function () {
-          console.log("done");
-        })
-        .catch(function (err) {
-          console.log(err);
-        });
-    };
- 
-  
+exports.reports_del = async (ip) => {
+  const app1 = await Ip.deleteOne({ IP: ip })
+    .then(function () {
+      console.log("done");
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+};
